f665de3698fb49abe9d9969c284f933ee511fc34
Goran Kekez <gorankekez@gmail.com>
2023-02-06 20:02:22 +0100

    add functionality

--- main.js
+++ main.js
@@ -1,2 +1,29 @@
 // Chuck Norris App
-console.log('Test')
+
+// All buttons
+const catBtn = document.querySelectorAll('.catBtn')
+// Search Text
+const searchText = document.getElementById('searchText')
+// Search Button
+const searchBtn = document.getElementById('searchBtn')
+// Joke Display
+const jokeDisplay = document.getElementById('jokeDisplay')
+
+//Get Joke Function
+function getJoke(url) {
+  fetch(url)
+    .then((prom) => prom.json())
+    .then((res) => (jokeDisplay.innerHTML = res.value))
+}
+
+// Category Joke
+function addEvent() {
+  catBtn.forEach((cat) =>
+    cat.addEventListener('click', () => {
+      joke = cat.id
+      let url = `https://api.chucknorris.io/jokes/random?category=${joke}`
+      getJoke(url)
+    })
+  )
+}
+addEvent()
